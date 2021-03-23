e5881021e7858b23f41c294e3951a8f35c470c0f
gtata <grzegorz.tata@unitygroup.com>
2021-03-23 12:55:44 +0100

    init_Dev_ed_1702

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react"
+import './App.css';
+// importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList"
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  return (
+    <div className="App">
+      <header>
+        <h1>gtata's ToDo list</h1>
+      </header>
+      <Form />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
